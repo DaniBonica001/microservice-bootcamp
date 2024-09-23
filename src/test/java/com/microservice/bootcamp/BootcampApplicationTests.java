a58e4d80a587ef4135fc577489fcfbc18c81d531
Daniela Bonilla Caceres <danielabc_0106@hotmail.com>
2024-09-23 17:12:29 -0500

    init

---
+++ src/test/java/com/microservice/bootcamp/BootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
